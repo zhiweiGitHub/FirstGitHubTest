def8696678375ba45d934ab7ab627a91346dab68
zhiwei <zw_wangzhiwei@163.com>
2019-11-21 01:03:22 +0800

    version-1

---
+++ test1.java
@@ -0,0 +1,13 @@
+/**
+* File Name:test1.java
+* Author: 王志伟
+* Mail: zw_wangzhiwei@163.com 
+* Created Time: 2019年11月21日 星期四 01时02分57秒
+**/
+
+public class test1
+{
+    public void main(String args[]){
+        System.out.println("hello world!");
+    }
+}
